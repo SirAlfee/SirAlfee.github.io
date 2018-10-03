6fe0b8f05425d701df194e63216bad336f485ef6
Nicholas Moynihan <30583968+SirAlfee@users.noreply.github.com>
2018-10-03 11:05:17 -0400

    small fix
    wrost workspace ever but whats new

--- Scripts/Count_Down.js
+++ Scripts/Count_Down.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("dec 21, 2018 14:30:00").getTime();
+var countDownDate = new Date("oct 1, 2018 09:26:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function () {
@@ -23,6 +23,6 @@ var countdownfunction = setInterval(function () {
     // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(countdownfunction);
-        document.getElementById("demo").innerHTML = "RELEASED";
+        document.getElementById("demo").innerHTML = "TBD";
     }
 }, 1000);
