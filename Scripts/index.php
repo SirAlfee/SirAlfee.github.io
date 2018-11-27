d5aae146a328aab580427dcaf05661d6972f98c3
Nicholas Moynihan <30583968+SirAlfee@users.noreply.github.com>
2018-11-27 09:04:56 -0500

    Add files via upload

---
+++ Scripts/index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+$fp = fsockopen("udp://71.233.138.165", 7777, $errno, $errstr);
+
+if (!$fp) {
+    echo "ERROR: $errno - $errstr<br />\n";
+
+} else {
+
+    fwrite($fp, "\n");
+
+    echo fread($fp, 26);
+
+    fclose($fp);
+
+}
+
+?>
+
